fec2f23afa4a8eb28583d6bd7e5d0fea616e9c9a
Evelina <evelinfimova@gmail.com>
2020-11-04 20:56:07 +0300

    1 Version (app.js)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import * as nums from './numbers';
+const element = (<div>
+  Hello, World!
+</div>);
 
-const multi = nums.count * nums.length;
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          {multi}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
