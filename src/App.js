fa1b346f7faa07ddd08c33921f681945844ba402
Evelina <evelinfimova@gmail.com>
2020-11-04 12:44:19 +0300

    1 Version (app.js)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+const stringNew = 'Today is a great day';
+const numberOne = 55;
+const numberTwo = 23;
+const boolOne = true;
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +14,23 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={{
+          color: '#68c9e5'
+        }}>
+          Hello, World!
+        </p>
+        <div>
+          {stringNew}, {boolOne && 'It is true'}, {boolOne ? 'boolOne is true' : 'boolOne is false'}
+        </div>
+        <div>
+          {numberOne}, {numberOne - numberTwo}
+        </div>
+        <div>
+          {undefined}
+          {null}
+          {false}
+          {true}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
